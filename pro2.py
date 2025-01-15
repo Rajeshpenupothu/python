4ce3d195add0ad7d56af604190c9d720c729f1ae
rajeshpenupothu <rajeshpenupothu@gmail.com>
2025-01-15 12:19:11 +0530

    palindrome

---
+++ pro2.py
@@ -0,0 +1,6 @@
+a=input("Enter a palindrome number:")
+b=a[::-1]//::-1 slicing operation and -1 represents rading value or numbers in reere order  
+if(a==b):
+    print("it is plindrome")
+else:
+    print("it is not a palindrome")    
